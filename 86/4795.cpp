4a0021768c2a5f9acd037d9a8f8fe3489960e145
Klara1423 <2218278697@qq.com>
2023-01-14 20:55:37 +0800

    补题

--- 86/4795.cpp
+++ 86/4795.cpp
@@ -6,28 +6,25 @@ using namespace std;
 
 int main()
 {
-    int n = 0, m = 0;
+    int n = 0, m = 0, hang[N] = {0}, lie[N] = {0}, a = 0, b = 0;
     cin >> n >> m;
-    int sum = n * n, a[N][N] = {0};
 
     while (m--)
     {
         int x = 0, y = 0;
         cin >> x >> y;
-        for (int i = 1; i <= n; i++)
+
+        if (hang[x] == 0)
         {
-            if(a[x][i] == 0)
-            {
-                a[x][i] = 1;
-                sum--;
-            }
-            if(a[i][y] == 0)
-            {
-                a[i][y] = 1;
-                sum--;
-            }
+            hang[x] = 1;
+            a++;
         }
-        cout << sum << " ";
+        if (lie[y] == 0)
+        {
+            lie[y] = 1;
+            b++;
+        }
+        cout << (long long)(n - a) * (n - b) << " ";
     }
     return 0;
 }
