a3564811ade5769c24dd68c51865e3143444c589
Klara1423 <2218278697@qq.com>
2023-02-12 02:23:39 +0800

    补题

--- 90/c.cpp
+++ 90/c.cpp
@@ -10,25 +10,13 @@ int main()
     cin >> n >> k >> str;
     ans += str;
 
-    if(str[0] == str[n - 1])
-        for (int i = 1; i < k; i++)
-            ans += str.substr(1);
-    else
-        for (int i = 1; i < k; i++)
-            ans += str;
+    int pos = 0;
+    for (int i = 0; i < n; i++)
+        if (str.substr(0, i) == str.substr(n - i, i))
+            pos = i;
 
-    int pos = 0, num = 0;
-    for (int i = 0; i < ans.size(); i++)
-    {
-        if (ans.find(str, pos)!= -1)
-        {
-            num++;
-            if(num == k)
-                break;
-            pos = ans.find(str, pos + 1);
-        }
-    }
-    ans.erase(pos + n, ans.size());
+    for (int i = 1; i < k; i++)
+        ans += str.substr(pos);
 
     cout << ans;
     return 0;
